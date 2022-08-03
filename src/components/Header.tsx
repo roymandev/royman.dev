a250870dd46ed0701c8ca027d48ab66f98ff859d
Royman <royman.dev@gmail.com>
2022-08-04 00:29:07 +0700

    feat: make Header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
 import HeaderLink from '@/components/HeaderLink';
 
 const Header = () => (
-  <header className="relative mx-auto flex w-full max-w-6xl items-center p-8 uppercase tracking-widest">
+  <header className="relative mx-auto mt-14 flex w-full max-w-6xl items-center justify-around gap-3 p-8 uppercase tracking-widest md:mt-0">
     <hr className="absolute inset-x-0 border-stone-300" />
-    <nav className="z-10 flex flex-1 gap-2">
+    <nav className="z-10 md:flex-1">
       <HeaderLink href="/">Home</HeaderLink>
     </nav>
 
-    <HeaderLink href="/" className="text-3xl font-medium">
-      Royman
-    </HeaderLink>
+    <div className="absolute inset-x-0 -top-8 flex justify-center md:relative md:top-0">
+      <HeaderLink href="/" className="text-3xl font-medium">
+        Royman
+      </HeaderLink>
+    </div>
 
-    <div className="z-10 flex flex-1 justify-end">
+    <div className="z-10 flex md:flex-1 md:justify-end">
       <HeaderLink href="https://github.com/roymandev">Github</HeaderLink>
     </div>
   </header>
