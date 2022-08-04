3c1923d264ea2400e88a2ebf68dcfa13b276e6d6
Royman <royman.dev@gmail.com>
2022-08-05 03:45:28 +0700

    feat(data): add myApps

---
+++ src/data/myApps.ts
@@ -0,0 +1,16 @@
+import { AppData } from '@/types';
+
+export const myApps: AppData[] = [
+  {
+    title: 'My Notes',
+    description: 'Just a regular notes app.',
+    url: 'https://my-notes.royman.dev/',
+    techs: ['react', 'firebase', 'tailwindcss', 'typescript'],
+  },
+  {
+    title: 'Worship App',
+    description: '(WIP) Lyrics presentation web based app for Church',
+    url: 'https://worship-app.vercel.app/',
+    techs: ['nextjs', 'typescript', 'tailwindcss'],
+  },
+];
