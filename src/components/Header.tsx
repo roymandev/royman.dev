4191cea119944ca893277d9302a07198f0157ab7
Royman <royman.dev@gmail.com>
2022-08-06 02:39:49 +0700

    feat: Header change home with resume link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ const Header = () => (
   <header className="relative mx-auto mt-14 flex w-full max-w-6xl items-center justify-around gap-3 p-8 uppercase tracking-widest md:mt-0">
     <hr className="absolute inset-x-0 border-stone-300" />
     <nav className="z-10 md:flex-1">
-      <HeaderLink href="/">Home</HeaderLink>
+      <HeaderLink href="https://raw.githubusercontent.com/roymandev/roymandev/main/Resume.pdf">
+        Resume
+      </HeaderLink>
     </nav>
 
     <div className="absolute inset-x-0 -top-8 flex justify-center md:relative md:top-0">
