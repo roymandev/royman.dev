d6f6d0f7559e2bfaa63de5c54de07496e032f6fe
Royman <royman.dev@gmail.com>
2022-11-24 03:39:43 +0700

    feat: make index page dark

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import AppList from '@/components/AppList';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import CustomSeo from '@/components/utils/CustomSeo';
-import { myApps } from '@/data/myApps';
+import { appsData } from '@/data/appsData';
 import { getBasePageMeta } from '@/lib/getPageMeta';
 import type { NextPage } from 'next';
 
@@ -19,24 +19,27 @@ const Home: NextPage = () => {
 
       <Header />
 
-      <main className="mx-auto flex max-w-4xl flex-col gap-10">
-        <section className="relative grid h-[80vh] place-content-center text-center text-xl leading-relaxed md:text-2xl">
+      <main className="container mx-auto flex flex-col gap-10 px-2">
+        <section className="relative grid h-[80vh] place-content-center text-center text-lg leading-relaxed sm:text-xl md:text-2xl">
           <div className="absolute inset-0 flex items-center justify-center gap-14">
-            <span className="h-[40vh] w-px bg-stone-300"></span>
-            <span className="h-[60vh] w-px bg-stone-300"></span>
-            <span className="h-[40vh] w-px bg-stone-300"></span>
+            <span className="h-[40vh] w-px bg-zinc-600"></span>
+            <span className="h-[60vh] w-px bg-zinc-600"></span>
+            <span className="h-[40vh] w-px bg-zinc-600"></span>
           </div>
-          <div className="z-10 bg-white py-6 px-4 leading-snug md:py-8 md:leading-normal">
+          <div className="z-10 bg-zinc-900 py-6 px-4 leading-snug md:py-8 md:leading-normal">
             <p className="mb-4 text-3xl font-bold">Hi I&apos;m Royman</p>
             <p>I&apos;m a man who loves to learn and create things.</p>
             <p>Thats why i learning programming and making great apps.</p>
           </div>
         </section>
 
-        <section className="text-center">
-          <h2 className="mb-4 text-2xl font-bold">Applications</h2>
-          <p className="mb-10">Apps that i made, more app coming soon.</p>
-          <AppList apps={myApps} />
+        <section>
+          <h2 className="mb-4 text-center text-2xl font-bold">Applications</h2>
+          <p className="mb-10 text-center">
+            Apps that i made, more app coming soon.
+          </p>
+
+          <AppList apps={appsData} />
         </section>
       </main>
 
