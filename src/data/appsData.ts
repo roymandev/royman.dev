85655a569b53bc0bfd2a86ec5b6b9b6b71c2dc96
Royman <royman.dev@gmail.com>
2022-11-24 03:38:14 +0700

    feat(data): update appsData

--- src/data/appsData.ts
+++ src/data/appsData.ts
@@ -1,6 +1,6 @@
 import { AppData } from '@/types';
 
-export const myApps: AppData[] = [
+export const appsData: AppData[] = [
   {
     title: 'My Notes',
     description: 'Just a regular notes app.',
@@ -10,13 +10,17 @@ export const myApps: AppData[] = [
     imageUrl:
       'https://ik.imagekit.io/roymandev/Firefox_Screenshot_2022-08-29T20-00-35.385Z_astnavNLA.png?ik-sdk-version=javascript-1.4.3&updatedAt=1661808297336',
     liveUrl: 'https://my-notes.royman.dev/',
-    repo: 'https://github.com/roymandev/my-notes',
+    repoUrl: 'https://github.com/roymandev/my-notes',
   },
   {
     title: 'Worship App',
     description: '(WIP) Lyrics presentation web based app for Church',
-    liveUrl: 'https://worship-app.royman.dev/',
     techs: ['react', 'typescript', 'firebase', 'tailwindcss'],
-    repo: 'https://github.com/roymandev/worship-app',
+    iconUrl:
+      'https://raw.githubusercontent.com/roymandev/worship-app/main/public/Worship%20App.svg',
+    imageUrl:
+      'https://ik.imagekit.io/roymandev/Worship_App_Screenshot_sEYm5226yg.png?ik-sdk-version=javascript-1.4.3&updatedAt=1668975794020',
+    liveUrl: 'https://worship-app.royman.dev/',
+    repoUrl: 'https://github.com/roymandev/worship-app',
   },
 ];
