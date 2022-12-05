ae89286ef23622b93c4e5e05d08f4f94dea5a75a
Royman <royman.dev@gmail.com>
2022-12-06 06:32:29 +0700

    feat: add search feature to project list page

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,46 +1,114 @@
 import AppListItem from '@/components/AppListItem';
+import CustomButton from '@/components/CustomButton';
+import CustomInput from '@/components/CustomInput';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import CustomSeo from '@/components/utils/CustomSeo';
 import { getPosts } from '@/lib/mdx';
-import { ProjectPostHeader } from '@/types';
+import twclsx from '@/lib/twclsx';
+import { ProjectPostHeader, TechList } from '@/types';
 import { GetStaticProps, NextPage } from 'next';
+import { useDeferredValue, useEffect, useState } from 'react';
+import { HiMagnifyingGlass } from 'react-icons/hi2';
 
 export interface ProjectListPageProps {
   apps: ProjectPostHeader[];
 }
 
-const ProjectListPage: NextPage<ProjectListPageProps> = ({ apps }) => (
-  <>
-    <CustomSeo
-      title="Project List"
-      description="Showcase of my project"
-      slug="project"
-    />
+const ProjectListPage: NextPage<ProjectListPageProps> = ({ apps }) => {
+  const [techList, setTechList] = useState<TechList[]>([]);
+  const [query, setQuery] = useState('');
+  const defferedQuery = useDeferredValue(query);
+  const [filteredApps, setFilteredApps] = useState(apps);
+  const [techsFilter, setTechsFilter] = useState<TechList[]>([]);
 
-    <Header />
+  useEffect(() => {
+    const techs = apps
+      .reduce<TechList[]>((acc, app) => acc.concat(app.techs), [])
+      .filter((value, index, self) => self.indexOf(value) === index);
 
-    <main className="container mx-auto max-w-4xl py-8 px-6">
-      <section className="mx-auto mb-10">
-        <h1 className="mb-4 text-5xl font-bold">Projects</h1>
-        <p className="text-cyan-100/50">
-          Showcase of my project that I&apos;ve created using with my knowledge
-          in Frontend Development.
-        </p>
-      </section>
+    setTechList(techs);
+  }, [apps]);
 
-      <section className="mx-auto">
-        <section className="mt-4 grid gap-4 md:grid-cols-2">
-          {apps.map((app) => (
-            <AppListItem key={app.slug} {...app} />
-          ))}
+  useEffect(() => {
+    // Filter app by title and tech
+    const filteredApps = apps.filter(
+      (app) =>
+        app.title.toLowerCase().includes(defferedQuery.toLowerCase()) &&
+        techsFilter.every((tech) => app.techs.includes(tech)),
+    );
+
+    setFilteredApps(filteredApps);
+  }, [apps, techsFilter, defferedQuery]);
+
+  return (
+    <>
+      <CustomSeo
+        title="Project List"
+        description="Showcase of my project"
+        slug="project"
+      />
+
+      <Header />
+
+      <main className="container mx-auto max-w-4xl py-8 px-6">
+        <section className="mx-auto mb-10">
+          <h1 className="mb-4 text-5xl font-bold">Projects</h1>
+          <p className="text-cyan-100/50">
+            Showcase of my project that I&apos;ve created using with my
+            knowledge in Frontend Development.
+          </p>
         </section>
-      </section>
-    </main>
 
-    <Footer />
-  </>
-);
+        <section className="mx-auto">
+          <div className="relative">
+            <CustomInput
+              type="text"
+              className="w-full pr-11"
+              placeholder="Search projects"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <HiMagnifyingGlass className="absolute top-3 right-3 h-5 w-5 text-cyan-100/50" />
+          </div>
+
+          <div className="mt-4 flex flex-wrap items-center gap-1 text-sm">
+            <span className="flex h-7 items-center">Tech list:</span>
+            {techList.map((tech) => (
+              <CustomButton
+                key={tech}
+                className={twclsx(
+                  'h-7 px-2',
+                  techsFilter.includes(tech)
+                    ? 'bg-cyan-100/20 hover:bg-cyan-100/30'
+                    : 'text-cyan-100/50',
+                )}
+                disabled={!filteredApps.find((app) => app.techs.includes(tech))}
+                onClick={() => {
+                  if (techsFilter.includes(tech)) {
+                    setTechsFilter((prevTechs) =>
+                      prevTechs.filter((prevTech) => prevTech !== tech),
+                    );
+                  } else setTechsFilter((prevTechs) => [...prevTechs, tech]);
+                }}
+              >
+                {tech}
+              </CustomButton>
+            ))}
+          </div>
+
+          <section className="mt-4 grid gap-4 md:grid-cols-2">
+            {filteredApps.map((app) => (
+              <AppListItem key={app.slug} {...app} />
+            ))}
+          </section>
+        </section>
+      </main>
+
+      <Footer />
+    </>
+  );
+};
 
 export const getStaticProps: GetStaticProps<
   ProjectListPageProps
