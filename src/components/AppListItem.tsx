c979007f5cffb724c438d6d8383a122513e85e83
Royman <royman.dev@gmail.com>
2022-12-05 08:53:47 +0700

    feat: redesign AppListItem

--- src/components/AppListItem.tsx
+++ src/components/AppListItem.tsx
@@ -1,5 +1,4 @@
-import BaseLink from '@/components/BaseLink';
-import twclsx from '@/lib/twclsx';
+import CustomButton from '@/components/CustomButton';
 import { ProjectPostHeader } from '@/types';
 import Image from 'next/image';
 
@@ -11,24 +10,21 @@ const AppListItem = ({
   iconUrl,
   slug,
 }: AppListItemProps) => (
-  <BaseLink
-    href={`/project/${slug}`}
-    className={twclsx(
-      'flex w-[400px] gap-4 border border-dashed border-zinc-700 px-6 py-4 text-left transition-all hover:border-transparent hover:bg-zinc-800 hover:shadow',
-      iconUrl && 'px-4',
-    )}
+  <CustomButton
+    key={slug}
+    href={`project/${slug}`}
+    className="h-auto items-center gap-4 bg-cyan-100/5 p-4"
   >
     {iconUrl && (
       <div className="h-20 w-20 shrink-0">
         <Image src={iconUrl} alt={title} width="80" height="80" />
       </div>
     )}
-
     <div className="flex flex-col justify-center">
-      <h3 className="mb-1 text-lg font-bold text-zinc-300">{title}</h3>
-      <p className="text-base text-zinc-400">{description}</p>
+      <h3 className="mb-1 font-bold">{title}</h3>
+      <p className="text-base text-cyan-100/50">{description}</p>
     </div>
-  </BaseLink>
+  </CustomButton>
 );
 
 export default AppListItem;
