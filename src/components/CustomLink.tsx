545519759adf323568ae770edb8dca9e8bbb4115
Royman <royman.dev@gmail.com>
2022-12-06 06:29:52 +0700

    feat: add CustomLink focus style

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -4,7 +4,7 @@ import twclsx from '@/lib/twclsx';
 const CustomLink = ({ className, ...rest }: BaseLinkProps) => (
   <BaseLink
     className={twclsx(
-      'underline decoration-cyan-200/30 underline-offset-2 hover:text-cyan-100 hover:decoration-cyan-200/50',
+      'underline decoration-cyan-200/30 underline-offset-2 transition duration-300 hover:text-cyan-100 hover:decoration-cyan-200/50 focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-cyan-500/70',
       className,
     )}
     {...rest}
