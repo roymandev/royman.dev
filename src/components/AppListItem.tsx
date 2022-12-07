23ce8806797e520281b1674deb63aa0c1e105fc5
Royman <royman.dev@gmail.com>
2022-12-07 07:42:22 +0700

    feat: AppListItem change align

--- src/components/AppListItem.tsx
+++ src/components/AppListItem.tsx
@@ -13,14 +13,14 @@ const AppListItem = ({
   <CustomButton
     key={slug}
     href={`project/${slug}`}
-    className="h-auto items-center gap-4 bg-cyan-100/5 p-4"
+    className="h-auto items-start gap-4 bg-cyan-100/5 p-4"
   >
     {iconUrl && (
       <div className="h-20 w-20 shrink-0">
         <Image src={iconUrl} alt={title} width="80" height="80" />
       </div>
     )}
-    <div className="flex flex-col justify-center">
+    <div className="flex flex-col justify-center self-center">
       <h3 className="mb-1 font-bold">{title}</h3>
       <p className="text-base text-cyan-100/50">{description}</p>
     </div>
