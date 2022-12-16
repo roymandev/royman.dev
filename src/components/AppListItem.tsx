62f40780053e6bf45e6aadda5b61d6ad87619d9b
Royman <royman.dev@gmail.com>
2022-12-16 07:20:53 +0700

    feat: add AppListItem dark theme class

--- src/components/AppListItem.tsx
+++ src/components/AppListItem.tsx
@@ -6,7 +6,7 @@ const AppListItem = ({ title, description, iconUrl, slug }: Project) => (
   <CustomButton
     key={slug}
     href={`project/${slug}`}
-    className="h-auto items-start gap-4 bg-cyan-100/5 p-4"
+    className="h-auto items-start gap-4 p-4"
   >
     {iconUrl && (
       <div className="h-20 w-20 shrink-0">
@@ -14,8 +14,10 @@ const AppListItem = ({ title, description, iconUrl, slug }: Project) => (
       </div>
     )}
     <div className="flex flex-col justify-center self-center">
-      <h3 className="mb-1 font-bold">{title}</h3>
-      <p className="text-base text-cyan-100/50">{description}</p>
+      <h3 className="font-bold">{title}</h3>
+      <p className="text-base text-slate-600 transition-colors duration-500 dark:text-neutral-500">
+        {description}
+      </p>
     </div>
   </CustomButton>
 );
