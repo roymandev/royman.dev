004e3d76917d1a8cab0748712741fa92ae7dc69f
Royman <royman.dev@gmail.com>
2022-12-23 01:07:24 +0700

    feat: remove PillsCheckbox background when disabled

--- src/components/PillsCheckbox.tsx
+++ src/components/PillsCheckbox.tsx
@@ -23,7 +23,7 @@ const PillsCheckbox = <ItemType extends string>({
           'rounded-lg bg-slate-400/30 px-3 py-1 capitalize',
           'dark:bg-neutral-700/30 dark:disabled:text-neutral-600',
           'ring-2 ring-transparent focus:outline-none focus-visible:outline-none focus-visible:ring-indigo-500/90',
-          'disabled:bg-transparent disabled:text-slate-600',
+          'disabled:bg-transparent disabled:text-slate-600 dark:disabled:bg-transparent',
           selectedItems.includes(item)
             ? 'bg-slate-700 text-slate-100 dark:bg-neutral-300 dark:text-neutral-900'
             : 'hover:bg-slate-400 dark:hover:bg-neutral-700',
