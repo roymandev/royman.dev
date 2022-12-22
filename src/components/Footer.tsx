fbd173ceedaad5e1db7104348558ceb4ae07c399
Royman <royman.dev@gmail.com>
2022-12-22 23:40:12 +0700

    feat: add Footer className props

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
 import BaseLink from '@/components/BaseLink';
+import twclsx from '@/lib/twclsx';
 import { SiGithub, SiLinkedin } from 'react-icons/si';
 
-const Footer = () => (
-  <footer className="my-28 text-center">
+export type FooterProps = {
+  className?: string;
+};
+
+const Footer = ({ className }: FooterProps) => (
+  <footer className={twclsx('my-28 text-center', className)}>
     <section>
       <h2 className="mb-10 text-2xl font-bold">Find me on</h2>
       <div className="flex justify-center gap-6">
