3a7f052ea3fbdd2cd5816d29b055af3157e70a64
Royman <royman.dev@gmail.com>
2023-01-25 06:47:04 +0700

    feat(resume): update skills section

--- src/components/Resume/SectionSkills.tsx
+++ src/components/Resume/SectionSkills.tsx
@@ -1,18 +1,25 @@
 import BaseSection from '@/components/Resume/BaseSection';
-import { royman } from '@/data/resumeData';
 
 const SectionSkills = () => (
   <BaseSection title="Skills">
-    <p className="mb-2">
-      <span className="font-medium">Technologies:</span>
-      <br />
-      {royman.skills.tech.join(', ')}
-    </p>
-    <p>
-      <span className="font-medium">Programming Languages:</span>
-      <br />
-      {royman.skills.lang.join(', ')}
-    </p>
+    <div className="flex flex-col gap-2">
+      <p>Familiar with:</p>
+      <ul className="ml-8 list-disc">
+        <li>
+          <b>React.js</b> and <b>Next.js</b>
+        </li>
+        <li>
+          <b>TypeScript</b> and <b>JavaScript</b>
+        </li>
+        <li>
+          <b>Tailwind CSS</b>
+        </li>
+        <li>
+          <b>Firebase</b> and <b>Supabase</b>
+        </li>
+      </ul>
+      <p>for building web applications.</p>
+    </div>
   </BaseSection>
 );
 
